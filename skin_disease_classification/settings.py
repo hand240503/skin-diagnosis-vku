e145b0c933173d30a424fde991e2c8042f97ee44
dangha <nguyendangha2405@gmail.com>
2025-05-15 19:21:21 +0700

    add  mysql

--- skin_disease_classification/settings.py
+++ skin_disease_classification/settings.py
@@ -62,8 +62,12 @@ WSGI_APPLICATION = 'skin_disease_classification.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'skin_diagnosis_vku',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 

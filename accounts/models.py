09653f080089651a77e320ee4d29abadb7599a73
dangha <nguyendangha2405@gmail.com>
2025-05-19 21:39:12 +0700

    review type userprofile

--- accounts/models.py
+++ accounts/models.py
@@ -4,18 +4,19 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     USER_TYPES = (
-        ('patient', 'Bệnh nhân'),
-        ('doctor', 'Bác sĩ'),
-        ('admin', 'Quản trị viên'),
+        (0, 'Quản trị viên'),   # admin
+        (1, 'Bác sĩ'),          # doctor
+        (2, 'Bệnh nhân'),       # patient
     )
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    type = models.CharField(max_length=20, choices=USER_TYPES, default='patient')
-    ten = models.CharField(max_length=255, null=True, blank=True)  # <-- Thêm dòng này
+    type = models.PositiveSmallIntegerField(choices=USER_TYPES, default=2)
+    ten = models.CharField(max_length=255, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     phone = models.CharField(max_length=20, null=True, blank=True)
     activation_token = models.UUIDField(default=uuid.uuid4, null=True, blank=True)
     is_activated = models.BooleanField(default=False)
+
     def __str__(self):
         return f"{self.user.username} - {self.get_type_display()}"
 
